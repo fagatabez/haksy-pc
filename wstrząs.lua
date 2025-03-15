7fb91e18038326bdaae4dcc558dc991df9eef557
fagatabez <oliwierpruchnik@gmail.com>
2025-03-15 23:46:21 +0100

    Update wstrząs.lua

--- wstrząs.lua
+++ wstrząs.lua
@@ -2,4 +2,3 @@ game.Players.LocalPlayer.PlayerGui.ShakeUI.ShakeScript.Enabled = false
 game.Players.LocalPlayer.PlayerGui.ShakeUI2.ShakeScript.Enabled = false
 game.Players.LocalPlayer.PlayerGui.ShakeUIB.ShakeScript.Enabled = false
 game.Players.LocalPlayer.PlayerGui.ShakeUISKND.ShakeScript.Enabled = false
-game.Players.LocalPlayer.PlayerGui.Recoil.ShakeScript.Enabled = false
