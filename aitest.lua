da2aeb6e2f1938fbe729505cd7a66b25c20144d0
fagatabez <oliwierpruchnik@gmail.com>
2025-04-04 19:03:49 +0200

    Update aitest.lua

--- aitest.lua
+++ aitest.lua
@@ -15,7 +15,7 @@ local modes = {
 
 local modeVals = {
     "BlackoutHourVal", "BloodNightVal", "CalamityHourVal", "CarnageHourVal", "ChaosHourVal",
-    "DWPVal", "FinalHourVal", "FrozenDeathVal", "GlitchHourVal", "InfernoHourVal",
+    "DWPVal", "FinalHourVal", "FrozenDeathVal", "GlitchHourV", "InfernoHourVal",
     "LowtierVal", "MikeHourVal", "OLDBNVal", "PureInsanityVal", "ShadowHourVal", "VoidHourVal", "VisionHourVal",
     "ULTIMAVal", "SkyfallHourVal", "BloodBathVal", "BlueScreenDeathValue", "IceAgeV", "MeltdownVal", "ShadowPhase2",
     "ThalassophobiaV", "ULTIMAP2Val", "UltimateCalamityValue"
